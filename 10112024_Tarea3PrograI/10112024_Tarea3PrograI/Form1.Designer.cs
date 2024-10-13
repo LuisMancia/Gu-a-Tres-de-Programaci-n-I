3d045d21dec7717c6a99f36993e02d4e3676172d
Luis Mancia <terann95@gmail.com>
2024-10-12 23:26:07 -0600

    Subida de Archivos

---
+++ 10112024_Tarea3PrograI/10112024_Tarea3PrograI/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace _10112024_Tarea3PrograI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtParcial = new System.Windows.Forms.TextBox();
+            this.txtGuia = new System.Windows.Forms.TextBox();
+            this.txtCtrlLectura = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.poderacionGuia = new System.Windows.Forms.ComboBox();
+            this.poderacionCtrlLectura = new System.Windows.Forms.ComboBox();
+            this.txtPruebas = new System.Windows.Forms.Label();
+            this.mensajeError1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mensajeError2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mensajeError3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mensajeError4 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mensajeError5 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE ALUMNO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PARCIAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GUIA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CONTROL DE LECTURA";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(164, 19);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(178, 20);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtParcial
+            // 
+            this.txtParcial.Location = new System.Drawing.Point(164, 53);
+            this.txtParcial.Name = "txtParcial";
+            this.txtParcial.Size = new System.Drawing.Size(178, 20);
+            this.txtParcial.TabIndex = 5;
+            this.txtParcial.TextChanged += new System.EventHandler(this.txtParcial_TextChanged);
+            // 
+            // txtGuia
+            // 
+            this.txtGuia.Location = new System.Drawing.Point(164, 82);
+            this.txtGuia.Name = "txtGuia";
+            this.txtGuia.Size = new System.Drawing.Size(178, 20);
+            this.txtGuia.TabIndex = 6;
+            this.txtGuia.TextChanged += new System.EventHandler(this.txtGuia_TextChanged);
+            // 
+            // txtCtrlLectura
+            // 
+            this.txtCtrlLectura.Location = new System.Drawing.Point(164, 111);
+            this.txtCtrlLectura.Name = "txtCtrlLectura";
+            this.txtCtrlLectura.Size = new System.Drawing.Size(178, 20);
+            this.txtCtrlLectura.TabIndex = 7;
+            this.txtCtrlLectura.TextChanged += new System.EventHandler(this.txtCtrlLectura_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(564, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(652, 161);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 26);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "PORCENTAJES:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(607, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "60 %";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 36);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "CALCULAR PROMEDIO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "CONTACTO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(427, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "60 %";
+            // 
+            // poderacionGuia
+            // 
+            this.poderacionGuia.FormattingEnabled = true;
+            this.poderacionGuia.Items.AddRange(new object[] {
+            "20 %",
+            "40 %"});
+            this.poderacionGuia.Location = new System.Drawing.Point(387, 80);
+            this.poderacionGuia.Name = "poderacionGuia";
+            this.poderacionGuia.Size = new System.Drawing.Size(121, 21);
+            this.poderacionGuia.TabIndex = 15;
+            this.poderacionGuia.SelectedIndexChanged += new System.EventHandler(this.poderacionGuia_SelectedIndexChanged);
+            // 
+            // poderacionCtrlLectura
+            // 
+            this.poderacionCtrlLectura.FormattingEnabled = true;
+            this.poderacionCtrlLectura.Items.AddRange(new object[] {
+            "20 %"});
+            this.poderacionCtrlLectura.Location = new System.Drawing.Point(387, 109);
+            this.poderacionCtrlLectura.Name = "poderacionCtrlLectura";
+            this.poderacionCtrlLectura.Size = new System.Drawing.Size(121, 21);
+            this.poderacionCtrlLectura.TabIndex = 16;
+            this.poderacionCtrlLectura.SelectedIndexChanged += new System.EventHandler(this.poderacionCtrlLectura_SelectedIndexChanged);
+            // 
+            // txtPruebas
+            // 
+            this.txtPruebas.AutoSize = true;
+            this.txtPruebas.Location = new System.Drawing.Point(364, 212);
+            this.txtPruebas.Name = "txtPruebas";
+            this.txtPruebas.Size = new System.Drawing.Size(0, 13);
+            this.txtPruebas.TabIndex = 17;
+            // 
+            // mensajeError1
+            // 
+            this.mensajeError1.ContainerControl = this;
+            // 
+            // mensajeError2
+            // 
+            this.mensajeError2.ContainerControl = this;
+            // 
+            // mensajeError3
+            // 
+            this.mensajeError3.ContainerControl = this;
+            // 
+            // mensajeError4
+            // 
+            this.mensajeError4.ContainerControl = this;
+            // 
+            // mensajeError5
+            // 
+            this.mensajeError5.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 237);
+            this.Controls.Add(this.txtPruebas);
+            this.Controls.Add(this.poderacionCtrlLectura);
+            this.Controls.Add(this.poderacionGuia);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtCtrlLectura);
+            this.Controls.Add(this.txtGuia);
+            this.Controls.Add(this.txtParcial);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GUIA #3";
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mensajeError5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtParcial;
+        private System.Windows.Forms.TextBox txtGuia;
+        private System.Windows.Forms.TextBox txtCtrlLectura;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox poderacionGuia;
+        private System.Windows.Forms.ComboBox poderacionCtrlLectura;
+        private System.Windows.Forms.Label txtPruebas;
+        private System.Windows.Forms.ErrorProvider mensajeError1;
+        private System.Windows.Forms.ErrorProvider mensajeError2;
+        private System.Windows.Forms.ErrorProvider mensajeError3;
+        private System.Windows.Forms.ErrorProvider mensajeError4;
+        private System.Windows.Forms.ErrorProvider mensajeError5;
+    }
+}
+
---
+++ 10112024_Tarea3PrograI/10112024_Tarea3PrograI/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10112024_Tarea3PrograI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Presentacion de las Variables
+        String nombre = string.Empty;
+        double notaParcial, notaGuia, notaCtrlLectura; //aqui se almacenan los valores de las notas
+        double porcentajeParcial = 0.60, porcentajeGuia, porcentajeCtrlLectura; // aqui se almacena el peso de la nota en el promedio final
+        double resultado;
+
+        //Almacenamiento del contenido ingresado en el textbox etiquetado como "Nombre"
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            nombre = txtNombre.Text;
+        }
+
+        //Este metodo deshabilita los campos del formulario Control de Lectura
+        private void deshabilitarCampos()
+        {
+            txtCtrlLectura.Enabled = false;
+            poderacionCtrlLectura.Enabled = false;
+        }
+
+        //Este metodo habilita los campos del formulario Control de Lectura
+        private void habilitarCampos()
+        {
+            txtCtrlLectura.Enabled = true;
+            poderacionCtrlLectura.Enabled = true;
+        }
+        
+        //Este metodo inhabilita los campos del formulario del control de lectura 
+        //y asigna valores por default en caso de estar deshabilitado para que el
+        //codigo no tome valores que se escriban en el formulario si este se encuentra
+        //apagado.
+        private void poderacionGuia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(poderacionGuia.SelectedIndex == 1)
+            {
+                deshabilitarCampos();
+                //si el porcentaje seleccionado es 40 % entonces el siguiente
+                //formulario se va a deshabilitar.
+                porcentajeGuia = 0.40;
+            }
+            else
+            {
+                habilitarCampos();
+                //si la guia vale 20 % entonces si se habilita el ingreso de nota del control de lectura
+                porcentajeGuia = 0.20;
+            }
+        }
+
+        //En este metodo se calcula el porcentage de nota segun lo que se haya seleccionado en el combobox
+        private void poderacionCtrlLectura_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(txtCtrlLectura.Enabled)
+            {
+                porcentajeCtrlLectura = 0.20;
+            } 
+            else
+            {
+                notaCtrlLectura = 0; // si el campo para ingresar la nota del control de lectura no esta habilitad
+                //entonces por default la nota seria cero, para no afectar el promedio final.
+                porcentajeCtrlLectura = 0;
+                txtCtrlLectura.Clear();
+            }
+        }
+
+        //Almacenamiento del contenido ingresado en el textbox etiquetado como "Parcial"
+        private void txtParcial_TextChanged(object sender, EventArgs e)
+        {
+            bool bandera = double.TryParse(txtParcial.Text, out notaParcial);
+            if (notaParcial >= 0 && notaParcial <=10 ) { do
+                {
+                    if (bandera)
+                    {
+                        mensajeError1.Clear();
+                        bandera = false;
+                    }
+                    else
+                    {
+                        mensajeError1.SetError(txtParcial, "Solo se permiten numeros.");
+                    }
+                } while (bandera);
+            }
+            else
+            {
+                mensajeError1.SetError(txtParcial, "El valor es invalido.");
+            }
+        }
+
+        //Almacenamiento del contenido ingresado en el textbox etiquetado como "Guia"
+        private void txtGuia_TextChanged(object sender, EventArgs e)
+        {
+            bool bandera1 = double.TryParse(txtGuia.Text, out notaGuia);
+            if (notaGuia>=0 && notaGuia<=10) {
+                do
+                {
+                    if (bandera1)
+                    {
+                        mensajeError2.Clear();
+                        bandera1 = false;
+                    }
+                    else
+                    {
+                        mensajeError2.SetError(txtGuia, "Solo se permiten numeros");
+                    }
+                } while (bandera1);
+            }
+            else
+            {
+                mensajeError2.SetError(txtGuia, "El valor es invalido.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogoResultado1 = MessageBox.Show("Desarrollado por " +"\n"+"Br. Luis Fernando Mancia Lopez"+"\n"+"Carnet: 27-3442-2023", "Acerca de mi", MessageBoxButtons.OK);
+            if (dialogoResultado1 == DialogResult.OK)
+            {
+                Limpieza();
+            }
+        }
+
+        //Almacenamiento del contenido ingresado en el textbox etiquetado como "Control de Lectura"
+        private void txtCtrlLectura_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCtrlLectura.Enabled) {
+                bool bandera2 = double.TryParse(txtCtrlLectura.Text, out notaCtrlLectura);
+                if (notaCtrlLectura >= 0 && notaCtrlLectura <= 10) {
+                    do
+                    {
+                        if (bandera2)
+                        {
+                            mensajeError3.Clear();
+                            bandera2 = false;
+                        }
+                        else
+                        {
+                            mensajeError3.SetError(txtCtrlLectura, "Solo se permiten numeros");
+                        }
+                    } while (bandera2);
+                }
+                else
+                {
+                    mensajeError3.SetError(txtCtrlLectura, "El valor es invalido.");
+                }
+            } else
+            {
+                txtCtrlLectura.Text = string.Empty;
+                notaCtrlLectura = 0;
+            }
+        }
+
+        private void Limpieza()
+        {
+            txtNombre.Clear();
+            txtParcial.Clear();
+            txtGuia.Clear();
+            txtCtrlLectura.Clear();
+            poderacionCtrlLectura.SelectedIndex = -1;
+            poderacionGuia.SelectedIndex = -1;
+            txtCtrlLectura.Enabled = true;
+            mensajeError1.Clear();
+            mensajeError2.Clear();
+            mensajeError3.Clear();
+            mensajeError4.Clear();
+            mensajeError5.Clear();
+            mensajeError5.Clear();
+        }
+        
+        private void mensajeBoton()
+        {
+            resultado = notaParcial * porcentajeParcial + notaGuia * porcentajeGuia + notaCtrlLectura * porcentajeCtrlLectura;
+            DialogResult dialogoResultado = MessageBox.Show("Promedio: " + resultado + "\n" + "Nombre: " + nombre, "RESULTADOS", MessageBoxButtons.OK);
+            if (dialogoResultado == DialogResult.OK)
+            {
+                Limpieza();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //El calculo del promedio solo se puede realizar si ya se selecciono el porcentage de la guia
+            //y en caso que la guia no tenga el 40 % entonces se verifica que el campo de nota y el
+            //porcentaje del control de lectura esten habilitados para hacer el calculo correspondiente.
+            //En caso que el campo de ingreso de nota del control de lectura este inhabilitado entonces
+            //se manda a llamar al metodo que genera el mensaje en pantalla.
+            if (poderacionGuia.SelectedIndex != -1) 
+            {
+                mensajeError4.Clear();
+                if (txtCtrlLectura.Enabled)
+                {
+                    if(poderacionCtrlLectura.SelectedIndex != -1)
+                    {
+                        mensajeError5.Clear();
+                        mensajeBoton();
+                    }
+                    else
+                    {
+                        mensajeError5.SetError(poderacionCtrlLectura, "Seleccione el porcentaje de la nota");
+                    }
+                }
+                else
+                {
+                    mensajeBoton();
+                }
+            }
+            else
+            {
+                mensajeError4.SetError(poderacionGuia, "Seleccione el porcentaje de la nota");
+            }
+        }
+    }
+}
